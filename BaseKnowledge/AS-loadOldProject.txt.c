9ecd1ea4eb77a1e7f50190ede468ba9afe3a3ea0
wujian1 <wujian1@chehejia.com>
2019-08-06 17:41:46 +0800

    2019-8-6 handler message

---
+++ BaseKnowledge/AS-loadOldProject.txt.c
@@ -0,0 +1,6 @@
+导入app demo项目
+
+1. 删除工程根目录下的.gradle、.idea、gradle
+2. 修改build.gradle下的gradle版本，以自己AS IDE新建的项目为准，classpath 'com.android.tools.build:gradle:3.4.0'，可以直接覆盖过去
+3. 把AS IDE新建的gradle目录复制过去
+4. 用AS IDE导入这个旧项目
